e74d950cd55422bf8e6aa0c9c0f3f6ab3246e925
newb9780-crypto <newb9780@gmail.com>
2026-05-05 10:36:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-api_id = int(os.environ.get("API_ID", 0))
-api_hash = os.environ.get("API_HASH", "")
-bot_token = os.environ.get("BOT_TOKEN", "")
+api_id = int(os.environ["API_ID"])
+api_hash = os.environ["API_HASH"]
+bot_token = os.environ["BOT_TOKEN"]
 auth_users = [1394117837]
 sudo_users = [1394117837]
